63ddec4e64d8bdd53849da3ceaf0bdb434756d3b
ATP-Pugaz <atppugazenthi@gmail.com>
2026-03-08 16:37:16 +0530

    Enhance server startup log messageadded port for render backend
    Added console log for server startup message.

--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
+
+
+app.listen(PORT, () => {
     console.log(`
 🚀 Money Manager Backend is running!
 📍 URL: http://localhost:${PORT}
